82dc38029e3451b0941488e4277053d841d8676b
DiegoHV <diegohurtado2412@gmail.com>
2020-11-27 16:11:49 -0400

    Base_Converter

---
+++ app/src/main/java/com/example/baseconverter/Soporte.java
@@ -0,0 +1,14 @@
+package com.example.baseconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Soporte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soporte);
+    }
+}
