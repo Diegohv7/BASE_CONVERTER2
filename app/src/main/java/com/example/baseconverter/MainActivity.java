d8861a59787778833b4c2b193315a8525a9ceeb2
DiegoHV <diegohurtado2412@gmail.com>
2020-11-30 15:39:42 -0400

    Base_Converter

--- app/src/main/java/com/example/baseconverter/MainActivity.java
+++ app/src/main/java/com/example/baseconverter/MainActivity.java
@@ -1,17 +1,45 @@
 package com.example.baseconverter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.soporte) {
+            Toast.makeText(this, "Soporte", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.contactanos) {
+            Toast.makeText(this, "Contactanos", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.reporte) {
+            Toast.makeText(this, "Reporte", Toast.LENGTH_SHORT).show();
+        }
+    return true;
     }
     //llamar a activity conversiones
     public void Siguiente(View view){
